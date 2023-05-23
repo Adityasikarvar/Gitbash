b523933e598ba241aa5cadc4ea61365ba81da4a6
Aditya Sikarvar <adityasikarvar2502@gmail.com>
2023-05-23 20:03:19 +0530

    Diff commit

---
+++ dif.java
@@ -0,0 +1,11 @@
+package Gitbash;
+
+public class dif {
+    public static void main(String[] args) {
+        int x =2;
+        int y=1;
+        int diff=x-y;
+        System.out.println(diff);
+
+    }
+}
