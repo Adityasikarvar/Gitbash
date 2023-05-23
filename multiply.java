df6de5006d480f729560786d7dae0e9c99c01fd8
Aditya Sikarvar <adityasikarvar2502@gmail.com>
2023-05-23 19:46:36 +0530

    ON MULTIPLY BRANCH.

---
+++ multiply.java
@@ -0,0 +1,10 @@
+package Gitbash;
+
+public class multiply {
+    public static void main(String[] args) {
+        int a=0;
+        int b=1;
+        int mul=a*b;
+        System.out.println(mul);
+    }
+}
